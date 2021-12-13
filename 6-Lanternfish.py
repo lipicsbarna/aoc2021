edaf17e42ef21b0527f375ab81fdfa27709657ff
lipicsbarna <lipics.barna@gmail.com>
2021-12-13 01:18:13 +0100

    kurva halak

---
+++ 6-Lanternfish.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+#sorry
+init_fishes = np.array([3,4,1,1,5,1,3,1,1,3,5,1,1,5,3,2,4,2,2,2,1,1,1,1,5,1,1,1,1,1,3,1,1,5,4,1,1,1,4,1,1,1,1,2,3,2,5,1,5,1,2,1,1,1,4,1,1,1,1,3,1,1,3,1,1,1,1,1,1,2,3,4,2,1,3,1,1,2,1,1,2,1,5,2,1,1,1,1,1,1,4,1,1,1,1,5,1,4,1,1,1,3,3,1,3,1,3,1,4,1,1,1,1,1,4,5,1,1,3,2,2,5,5,4,3,1,2,1,1,1,4,1,3,4,1,1,1,1,2,1,1,3,2,1,1,1,1,1,4,1,1,1,4,4,5,2,1,1,1,1,1,2,4,2,1,1,1,2,1,1,2,1,5,1,5,2,5,5,1,1,3,1,4,1,1,1,1,1,1,1,4,1,1,4,1,1,1,1,1,2,1,2,1,1,1,5,1,1,3,5,1,1,5,5,3,5,3,4,1,1,1,3,1,1,3,1,1,1,1,1,1,5,1,3,1,5,1,1,4,1,3,1,1,1,2,1,1,1,2,1,5,1,1,1,1,4,1,3,2,3,4,1,3,5,3,4,1,4,4,4,1,3,2,4,1,4,1,1,2,1,3,1,5,5,1,5,1,1,1,5,2,1,2,3,1,4,3,3,4,3])
+
+
+def examine_fucking_fishes(init_fishes: np.array, days: int) -> int:
+    _counts = np.unique(init_fishes, return_counts=True)
+    counts = dict(zip(_counts[0], _counts[1]))
+    for day in range(days):
+        new_counts = defaultdict(int)
+        
+        for num, count in counts.items():
+            
+            if num > 0:
+                new_idx = num - 1
+                new_counts[new_idx] += count
+            else:
+                new_counts[8] += count
+                new_counts[6] += count
+                
+        counts = new_counts
+        
+    return sum(counts.values())
+        
+    
+    
+# 1st result
+print(examine_lanternfish(init_fishes, 80))
+
+# 2nd result
+# Warning - runs long as hell
+print(examine_lanternfish(init_fishes, 256))
